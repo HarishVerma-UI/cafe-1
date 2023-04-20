db3e40475eec50b20c3a4bc364cf1d87ae4ec37c
Harish verma <hemantdixit@hemants-MacBook-Air.local>
2023-04-20 11:55:58 +0530

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeftComponent } from './body/left/left.component';
+import { CenterComponent } from './body/center/center.component';
+import { RightComponent } from './body/right/right.component';
+import { TableComponent } from './pages/table/table.component';
+import { AlertsComponent } from './pages/alerts/alerts.component';
+import { ImagesComponent } from './pages/images/images.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    LeftComponent,
+    CenterComponent,
+    RightComponent,
+    TableComponent,
+    AlertsComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
